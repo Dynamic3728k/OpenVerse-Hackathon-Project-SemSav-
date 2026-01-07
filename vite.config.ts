46df140c80241e6b7f416b81cb173d22d01882f0
GULSHAN KUMAR <gulshan.nita@gmail.com>
2026-01-07 23:13:34 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
     return {
-      
-      base: '/', 
+      base:'/',
       
       server: {
         port: 3000,
